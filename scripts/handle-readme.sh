27a3fa1676e3f35cb3d9ec773f148fcb4220bc81
Jolan Gerkens <173156207+olankens@users.noreply.github.com>
2026-09-25 16:31:13 +0200

    chore: scale readme feature grid icons to fill cell width fully

--- scripts/handle-readme.sh
+++ scripts/handle-readme.sh
@@ -5,14 +5,14 @@ set -euo pipefail
 DIR="$(cd "$(dirname "$0")" && pwd)"
 RME="$DIR/../README.md"
 SRC="$DIR/../source"
-MAX=5
+MAX=6
 
 ALL=("$SRC"/*/*.png)
 TXT="<table>"
 for NUM in "${!ALL[@]}"; do
 	((NUM % MAX == 0)) && TXT="${TXT}$([ "$NUM" -ne 0 ] && echo '</tr></tbody>')<tbody><tr>" || true
 	FLD=$(basename "$(dirname "${ALL[$NUM]}")")
-	TXT="${TXT}<td align=\"center\" width=\"99999\">&nbsp;<div><a href=\"source/${FLD}/${FLD}.icns\"><img src=\"source/${FLD}/${FLD}.png\" align=\"center\" width=\"56\"></a></div>&nbsp;</td>"
+	TXT="${TXT}<td align=\"center\" width=\"99999\">&nbsp;<div><a href=\"source/${FLD}/${FLD}.icns\"><img src=\"source/${FLD}/${FLD}.png\" align=\"center\" width=\"100%\"></a></div>&nbsp;</td>"
 done
 TXT="${TXT}</tr></tbody></table>"
 
